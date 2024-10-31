ed21943cd41325d9289c3969f5763f4e4b49fa22
LukeMed <luke.lam1988@gmail.com>
2024-10-31 15:38:39 -0300

    Desafio surpresa

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+let nomePokemon = ["Poochyena", "Zigzagoon", "Dragonite", "Hurmple"]
+let timePokemon = [
 
-let Alimentos = ["Açucar", "Café", "Suco", "Bolacha"]
+    ["Poochyena", "F" , 3],
+    ["Poochyena", "M" , 2],
+    ["Zigzagoon", "F", 2],
+    ["Dragonite", "M", 5],
+    ["Dragonite", "F", 5],
+    ["Hurmple", "M" , 2]
 
-console.log(Alimentos)
+]
+const CadastroEfetuado = " foi cadastrado com sucesso."
+
+console.log("O pokemon " + timePokemon [0][0] + " do sexo " + timePokemon [0][1] + " que está no nível " + timePokemon [0][2] + CadastroEfetuado)
+console.log("O pokemon " + timePokemon [1][0] + " do sexo " + timePokemon [1][1] + " que está no nível " + timePokemon [1][2] + CadastroEfetuado)
+console.log("O pokemon " + timePokemon [2][0] + " do sexo " + timePokemon [2][1] + " que está no nível " + timePokemon [2][2] + CadastroEfetuado)
+console.log("O pokemon " + timePokemon [3][0] + " do sexo " + timePokemon [3][1] + " que está no nível " + timePokemon [3][2] + CadastroEfetuado)
+console.log("O pokemon " + timePokemon [4][0] + " do sexo " + timePokemon [4][1] + " que está no nível " + timePokemon [4][2] + CadastroEfetuado)
+console.log("O pokemon " + timePokemon [5][0] + " do sexo " + timePokemon [5][1] + " que está no nível " + timePokemon [5][2] + CadastroEfetuado)
